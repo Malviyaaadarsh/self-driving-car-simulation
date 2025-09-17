3900cadb7ca290c3cb7c4b7a68dd5f1919c14110
Aadarsh Malviya <165465476+Malviyaaadarsh@users.noreply.github.com>
2025-09-17 17:16:37 +0530

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -1,2 +1,13 @@
-const canvas = document.getElementById("canvas1");
-canvas.width = 300;
+const canvas = document.getElementById("road-canvas");
+canvas.width = 400;
+const context = canvas.getContext("2d");
+const road = new Road(canvas.width / 2, canvas.width * 0.9, 5);
+const car = new Car(road.getLaneCenter(2), 450, 25, 50);
+animate();
+function animate() {
+    car.update();
+    canvas.height = 600;
+    road.draw(context);
+    car.draw(context);
+    requestAnimationFrame(animate);
+}
